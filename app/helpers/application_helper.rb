09f4a48ffba226a39fffe0d8e0fdb068712bff9b
qsat <araisat@gmail.com>
2012-11-15 20:46:14 +0900

    ensyuu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
             "#{base_title} | #{@title}"
         end
     end
+    
+    def logo
+        logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+        link_to logo, root_path
+    end
+        
 end
